6b7955ff006157bcda040909f658f255c82ad8d8
vidapogosoft <vidapogosoft@hotmail.com>
2024-08-06 11:43:22 -0500

    Update CategoriaController.cs

--- dia11/MVCAppv2/Controllers/CategoriaController.cs
+++ dia11/MVCAppv2/Controllers/CategoriaController.cs
@@ -1,78 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
-using Microsoft.AspNetCore.Http;
-
-using System.Text;
-using System.Net.Http;
-using System.Net.Http.Headers;
-
-using Newtonsoft.Json;
-
 using MVCAppv2.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace MVCAppv2.Controllers
 {
-    public class CategoriaController : Controller
-    {
-        string BaseURL = "http://localhost:5244/";
+	public class CategoriaController : Controller
+	{
+		string BaseURL = "http://localhost:5244/";
 
-        public async Task<ActionResult> Index()
-        {
-            //defino la lista
-            var ListCategorias = new List<Categoria>();
+		public async Task<ActionResult> Index()
+		{
+			//defino la lista
+			var ListCategorias = new List<Categoria>();
 
-            //llenar esta lista
-            using (var client = new HttpClient())
-            {
-                //call api backend
+			//llenar esta lista
+			using (var client = new HttpClient())
+			{
+				//call api backend
 
-                client.BaseAddress = new Uri(BaseURL);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				client.BaseAddress = new Uri(BaseURL);
+				client.DefaultRequestHeaders.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                HttpResponseMessage res = await client.GetAsync("api/Categorias");
+				HttpResponseMessage res = await client.GetAsync("api/Categorias");
 
 
-                if(res.IsSuccessStatusCode)
-                {
-                    var Response = res.Content.ReadAsStringAsync().Result;
+				if (res.IsSuccessStatusCode)
+				{
+					var Response = res.Content.ReadAsStringAsync().Result;
 
-                    ListCategorias = JsonConvert.DeserializeObject<List<Categoria>>(Response);
-                }
+					ListCategorias = JsonConvert.DeserializeObject<List<Categoria>>(Response);
+				}
 
-            }
+			}
 
-            return View(ListCategorias);
-        }
+			return View(ListCategorias);
+		}
 
-        public async Task<ActionResult> Details(int IdCategoria)
-        {
+		public async Task<ActionResult> Details(int IdCategoria)
+		{
 
-            var categoria = new Categoria();
-			
-            using (var client = new HttpClient())
-            {
+			var categoria = new Categoria();
+
+			using (var client = new HttpClient())
+			{
 				client.BaseAddress = new Uri(BaseURL);
 				client.DefaultRequestHeaders.Clear();
 
 				HttpResponseMessage res = await client.GetAsync("api/Categorias/id/" + IdCategoria);
 
-                if (res.IsSuccessStatusCode)
-                {
-                    var Response = res.Content.ReadAsStringAsync().Result;
+				if (res.IsSuccessStatusCode)
+				{
+					var Response = res.Content.ReadAsStringAsync().Result;
 
-                    categoria = JsonConvert.DeserializeObject<Categoria>(Response);
-                }
+					categoria = JsonConvert.DeserializeObject<Categoria>(Response);
+				}
 
-		    }
+			}
 
-		    return View(categoria);
-        }
+			return View(categoria);
+		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public async Task<ActionResult> Details(Categoria delitem)
-        {
+		{
 			try
 			{
 				using (var client = new HttpClient())
@@ -101,51 +95,51 @@ namespace MVCAppv2.Controllers
 
 
 		public ActionResult Create()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
 		public ActionResult Create(Categoria newitem)
-        {
-            try
-            {
-                newitem.Idcategoria = 0;
-                newitem.Estado = true;
+		{
+			try
+			{
+				newitem.Idcategoria = 0;
+				newitem.Estado = true;
 
 				using (var client = new HttpClient())
-                {
+				{
 
 					client.BaseAddress = new Uri(BaseURL);
 					client.DefaultRequestHeaders.Clear();
 					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                    var json = JsonConvert.SerializeObject(newitem);
-                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+					var json = JsonConvert.SerializeObject(newitem);
+					var content = new StringContent(json, Encoding.UTF8, "application/json");
 
 					HttpResponseMessage res = client.PostAsync("api/Categorias/registro/", content).GetAwaiter().GetResult();
 
-                    if (res.IsSuccessStatusCode)
-                    {
-                        var Response = res.Content.ReadAsStringAsync().Result;
-                    }
+					if (res.IsSuccessStatusCode)
+					{
+						var Response = res.Content.ReadAsStringAsync().Result;
+					}
 
 				}
 
 				return RedirectToAction("Index");
 
-            }
-            catch
-            {
+			}
+			catch
+			{
 
-                return View();
-            }
+				return View();
+			}
 
-        }
+		}
 
-        public async Task<ActionResult> Edit(int IdCategoria)
-        {
+		public async Task<ActionResult> Edit(int IdCategoria)
+		{
 			var categoria = new Categoria();
 
 			using (var client = new HttpClient())
@@ -171,7 +165,7 @@ namespace MVCAppv2.Controllers
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public ActionResult Edit(Categoria edititem)
-        {
+		{
 			try
 			{
 				edititem.Estado = true;
