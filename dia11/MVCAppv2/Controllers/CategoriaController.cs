0392b8ce416c73153e144ec8cbb619f8c45c3e86
vidapogosoft <vidapogosoft@hotmail.com>
2024-08-06 11:24:22 -0500

    taller mvc

--- dia11/MVCAppv2/Controllers/CategoriaController.cs
+++ dia11/MVCAppv2/Controllers/CategoriaController.cs
@@ -69,5 +69,141 @@ namespace MVCAppv2.Controllers
 		    return View(categoria);
         }
 
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Details(Categoria delitem)
+        {
+			try
+			{
+				using (var client = new HttpClient())
+				{
+
+					client.BaseAddress = new Uri(BaseURL);
+					client.DefaultRequestHeaders.Clear();
+				
+					HttpResponseMessage res = await client.DeleteAsync("api/Categorias/delete2/" + delitem.Idcategoria);
+
+					if (res.IsSuccessStatusCode)
+					{
+						var Response = res.Content.ReadAsStringAsync().Result;
+					}
+				}
+
+				return RedirectToAction("Index");
+
+			}
+			catch
+			{
+
+				return View();
+			}
+		}
+
+
+		public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public ActionResult Create(Categoria newitem)
+        {
+            try
+            {
+                newitem.Idcategoria = 0;
+                newitem.Estado = true;
+
+				using (var client = new HttpClient())
+                {
+
+					client.BaseAddress = new Uri(BaseURL);
+					client.DefaultRequestHeaders.Clear();
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var json = JsonConvert.SerializeObject(newitem);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+					HttpResponseMessage res = client.PostAsync("api/Categorias/registro/", content).GetAwaiter().GetResult();
+
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var Response = res.Content.ReadAsStringAsync().Result;
+                    }
+
+				}
+
+				return RedirectToAction("Index");
+
+            }
+            catch
+            {
+
+                return View();
+            }
+
+        }
+
+        public async Task<ActionResult> Edit(int IdCategoria)
+        {
+			var categoria = new Categoria();
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri(BaseURL);
+				client.DefaultRequestHeaders.Clear();
+
+				HttpResponseMessage res = await client.GetAsync("api/Categorias/id/" + IdCategoria);
+
+				if (res.IsSuccessStatusCode)
+				{
+					var Response = res.Content.ReadAsStringAsync().Result;
+
+					categoria = JsonConvert.DeserializeObject<Categoria>(Response);
+				}
+
+			}
+
+			return View(categoria);
+
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(Categoria edititem)
+        {
+			try
+			{
+				edititem.Estado = true;
+
+				using (var client = new HttpClient())
+				{
+
+					client.BaseAddress = new Uri(BaseURL);
+					client.DefaultRequestHeaders.Clear();
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+					var json = JsonConvert.SerializeObject(edititem);
+					var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+					HttpResponseMessage res = client.PutAsync("api/Categorias/upd1/", content).GetAwaiter().GetResult();
+
+					if (res.IsSuccessStatusCode)
+					{
+						var Response = res.Content.ReadAsStringAsync().Result;
+					}
+
+				}
+
+				return RedirectToAction("Index");
+
+			}
+			catch
+			{
+
+				return View();
+			}
+		}
+
 	}
 }
